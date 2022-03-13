87e1107297fcddec311f60d2c17ec0c122632fae
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-03-13 19:48:51 +0530

    Added react router

--- fitbit-react/src/Components/Navbar/Navbar.js
+++ fitbit-react/src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./navbar.css";
 import { SearchBar, PrimaryButton, Logo, NavbarIcons, GrMenu } from "./index";
-
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
