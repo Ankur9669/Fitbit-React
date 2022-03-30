728eb962f0b9c421a7e1bdb37b314a52bfa80518
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-03-30 20:11:19 +0530

    npm run build and done with the changes suggested by peers

--- fitbit-react/src/Pages/Wishlist/WishList.js
+++ fitbit-react/src/Pages/Wishlist/WishList.js
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import VerticalCard from "../../Components/VerticalCard/VerticalCard";
+import { useUser } from "../../Context/user-context";
 import { useWishList } from "../../Context/wishlist-context";
-import { Navbar, Footer, AiFillHeart } from "./index";
+import { Navbar, Footer, AiFillHeart, PrimaryButton } from "./index";
+import { Link } from "react-router-dom";
 import "./wishlist.css";
+
 function WishList() {
   const { wishlist } = useWishList();
+  const { user } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    !user.isUserLoggedIn && navigate("/login");
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="spacer-3"></div>
-      <section class="wishlist-products">
-        <h3 class="h2 centered-text">My WishList</h3>
-        <div class="spacer-3"></div>
-        <div class="wishlist-products-container">
-          {wishlist?.map((wishListProduct) => {
-            return (
-              <VerticalCard
-                key={wishListProduct?._id}
-                product={wishListProduct}
-              />
-            );
-          })}
+      {wishlist.length > 0 ? (
+        <section className="wishlist-products">
+          <h3 className="h2 centered-text">My WishList</h3>
+          <div className="spacer-3"></div>
+          <div className="wishlist-products-container">
+            {wishlist?.map((wishListProduct) => {
+              return (
+                <VerticalCard
+                  key={wishListProduct?._id}
+                  product={wishListProduct}
+                />
+              );
+            })}
+          </div>
+        </section>
+      ) : (
+        <div className="wishlist-cart-container">
+          <img
+            src="/images/empty-wishlist.svg"
+            className="wishlist-cart-img img-responsive"
+            alt="empty-wishlist"
+          ></img>
+          <p className="weight-semi-bold font-large centered-text">
+            Your WishList is Empty
+          </p>
+          <div className="spacer-1"></div>
+          <p className="weight-normal font-medium centered-text">
+            Add Some Items Now
+          </p>
+          <div className="spacer-1"></div>
+          <Link to="/products">
+            <PrimaryButton buttonText="Add Items" />
+          </Link>
+
+          <div className="spacer-3"></div>
         </div>
-      </section>
+      )}
+
       <div className="spacer-3"></div>
       <Footer />
     </div>
--- fitbit-react/src/Pages/Wishlist/index.js
+++ fitbit-react/src/Pages/Wishlist/index.js
@@ -1,5 +1,6 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import { AiFillHeart } from "../../Assets/icons";
+import { PrimaryButton } from "../../Components/Navbar";
 
-export { Navbar, Footer, AiFillHeart };
+export { Navbar, Footer, AiFillHeart, PrimaryButton };
