f9c0f09608bdedf8b3943fec1a1491cdd6074090
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-07 02:59:42 +0530

    done with the changes suggested by peers

--- fitbit-react/src/Pages/Homepage/FeaturedProducts/FeaturedProductsCard.js
+++ fitbit-react/src/Pages/Homepage/FeaturedProducts/FeaturedProductsCard.js
@@ -3,12 +3,19 @@ import { BsFillHeartFill } from "../../../Assets/icons";
 import { Link } from "react-router-dom";
 
 function FeaturedProductsCard({ product, redirectUrl = "/products" }) {
+  const {
+    productImageUrl,
+    productTitle,
+    discountedPrice,
+    realPrice,
+    discountPercent,
+  } = product;
   return (
     <Link to={redirectUrl} className="card card-vertical card-hover">
       <div className="image-container">
         <img
           alt="card-image"
-          src={product.productImageUrl}
+          src={productImageUrl}
           className="img-responsive"
           loading="lazy"
         />
@@ -16,17 +23,17 @@ function FeaturedProductsCard({ product, redirectUrl = "/products" }) {
       <div className="wrapper">
         <div className="text-container">
           <h5 className="font-medium-large weight-semi-bold primary-text-color card-vertical-heading">
-            {product.productTitle}
+            {productTitle}
             <BsFillHeartFill style={{ fontSize: "1.4rem" }} />
           </h5>
           <div className="margin-vertical-xs"></div>
           <p className="font-medium weight-semi-bold primary-text-color">
-            &#8377;{product.discountedPrice}&nbsp;
+            &#8377;{discountedPrice}&nbsp;
             <span className="font-extra-small weight-semi-bold strike-through secondary-text-color">
-              &#8377;{product.realPrice}&nbsp;
+              &#8377;{realPrice}&nbsp;
             </span>
             <span className="text-badge text-badge-rounded-border text-badge-small discount-percent-horizontal">
-              {product.discountPercent} % off
+              {discountPercent} % off
             </span>
           </p>
         </div>
